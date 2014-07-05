a79d43b30d4977cd0f74e6e0fa1e46934b981a38
Denis Pauk <pauk.denis@gmail.com>
2014-07-05 18:55:06 +0300

    restore documentation from protodug source code

--- include/linux/i2c/atmel_mxt_ts.h
+++ include/linux/i2c/atmel_mxt_ts.h
@@ -26,13 +26,16 @@
 #define MXT_ROTATED_180		0x6
 #define MXT_DIAGONAL_COUNTER	0x7
 
+/* MXT_TOUCH_KEYARRAY_T15 */
 #define MXT_KEYARRAY_MAX_KEYS	32
 
+/* Bootoader IDs */
 #define MXT_BOOTLOADER_ID_224		0x0A
 #define MXT_BOOTLOADER_ID_224E		0x06
 #define MXT_BOOTLOADER_ID_1386		0x01
 #define MXT_BOOTLOADER_ID_1386E		0x10
 
+/* Config data for a given maXTouch controller with a specific firmware */
 struct mxt_config_info {
 	const u8 *config;
 	size_t config_length;
@@ -41,7 +44,7 @@ struct mxt_config_info {
 	u8 version;
 	u8 build;
 	u8 bootldr_id;
-	
+	/* Points to the firmware name to be upgraded to */
 	const char *fw_name;
 };
 
@@ -50,13 +53,13 @@ struct mxt_platform_data {
 	const struct mxt_config_info *config_array;
 	size_t config_array_size;
 
-	
+	/* touch panel's minimum and maximum coordinates */
 	u32 panel_minx;
 	u32 panel_maxx;
 	u32 panel_miny;
 	u32 panel_maxy;
 
-	
+	/* display's minimum and maximum coordinates */
 	u32 disp_minx;
 	u32 disp_maxx;
 	u32 disp_miny;
