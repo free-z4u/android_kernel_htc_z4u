fefc1aa9c7dd7936a9d6620060211efcfaf0095c
Denis Pauk <pauk.denis@gmail.com>
2016-04-16 12:38:18 +0300

    fix atags export

--- arch/arm/kernel/atags.c
+++ arch/arm/kernel/atags.c
@@ -1,33 +1,35 @@
-#include <linux/slab.h>
 #include <linux/proc_fs.h>
+#include <linux/uaccess.h>
 #include <asm/setup.h>
-#include <asm/types.h>
-#include <asm/page.h>
 
-struct buffer {
-	size_t size;
-	char data[];
-};
+static size_t atags_size = 0;
+static char * atags_buf;
 
-static int
-read_buffer(char* page, char** start, off_t off, int count,
-	int* eof, void* data)
+static ssize_t atags_read(struct file *file, char __user *buf,
+			  size_t len, loff_t *offset)
 {
-	struct buffer *buffer = (struct buffer *)data;
+	ssize_t count;
+	loff_t pos = *offset;
 
-	if (off >= buffer->size) {
-		*eof = 1;
+	if (!atags_buf)
 		return 0;
-	}
 
-	count = min((int) (buffer->size - off), count);
+	if (pos >= atags_size)
+		return 0;
 
-	memcpy(page, &buffer->data[off], count);
+	count = min(len, (size_t)(atags_size - pos));
+	if (copy_to_user(buf, atags_buf + pos, count))
+		return -EFAULT;
 
+	*offset += count;
 	return count;
 }
 
-#define BOOT_PARAMS_SIZE 1536
+static const struct file_operations atags_file_ops = {
+	.read = atags_read,
+};
+
+#define BOOT_PARAMS_SIZE (1 << 14)
 static char __initdata atags_copy[BOOT_PARAMS_SIZE];
 
 void __init save_atags(const struct tag *tags)
@@ -43,40 +45,46 @@ static int __init init_atags_procfs(void)
 	 */
 	struct proc_dir_entry *tags_entry;
 	struct tag *tag = (struct tag *)atags_copy;
-	struct buffer *b;
 	size_t size;
 
 	if (tag->hdr.tag != ATAG_CORE) {
-		printk(KERN_INFO "No ATAGs?");
+		pr_info("No ATAGs?");
 		return -EINVAL;
 	}
 
-	for (; tag->hdr.size; tag = tag_next(tag))
-		;
+	for (; tag->hdr.size; tag = tag_next(tag)) {
+		size = (char*)tag_next(tag) - (char*)atags_copy;
+		if (size > sizeof(atags_copy)) {
+			pr_info("Atags bigger than buffer!");
+			return -EINVAL;
+		}
+	}
 
 	/* include the terminating ATAG_NONE */
-	size = (char *)tag - atags_copy + sizeof(struct tag_header);
+	atags_size = (char *)tag - (char *)atags_copy + sizeof(struct tag_header);
 
 	WARN_ON(tag->hdr.tag != ATAG_NONE);
 
-	b = kmalloc(sizeof(*b) + size, GFP_KERNEL);
-	if (!b)
+	atags_buf = kmalloc(atags_size, GFP_KERNEL);
+	if (!atags_buf)
 		goto nomem;
 
-	b->size = size;
-	memcpy(b->data, atags_copy, size);
+	memcpy(atags_buf, atags_copy, atags_size);
 
-	tags_entry = create_proc_read_entry("atags", 0400,
-			NULL, read_buffer, b);
+	tags_entry = create_proc_entry("atags", S_IFREG | S_IRUGO, NULL);
 
 	if (!tags_entry)
 		goto nomem;
 
+	tags_entry->proc_fops = &atags_file_ops;
+	tags_entry->uid = 0;
+	tags_entry->gid = 0;
+	tags_entry->size = atags_size;
 	return 0;
 
 nomem:
-	kfree(b);
-	printk(KERN_ERR "Exporting ATAGs: not enough memory\n");
+	kfree(atags_buf);
+	pr_err("Exporting ATAGs: not enough memory\n");
 
 	return -ENOMEM;
 }
